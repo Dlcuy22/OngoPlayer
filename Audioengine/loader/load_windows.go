06a6f5283507e6beef76da814945af8484d20180
Dlcuy22 <alifqwik@gmail.com>
2026-07-09 07:35:46 +0700

    ci: fix build erorr

--- Audioengine/loader/load_windows.go
+++ Audioengine/loader/load_windows.go
@@ -43,6 +43,14 @@ func Load(name string) (uintptr, error) {
 	return uintptr(handle), err
 }
 
+func LoadWithPath(name string) (uintptr, string, error) {
+	handle, err := syscall.LoadLibrary(name)
+	if err != nil {
+		return 0, "", fmt.Errorf("%s: error loading library: %w", name, err)
+	}
+	return uintptr(handle), name, nil
+}
+
 func Get(lib uintptr, name string) uintptr {
 	addr, err := syscall.GetProcAddress(syscall.Handle(lib), name)
 	if err != nil {
